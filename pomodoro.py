1b099cc4abb6865455b68644b840fe483c646939
Johannes Huwald <=>
2020-05-15 12:33:21 +0200

    remaining time of pomodoro timer is shown in title of terminal. (Breaks tqdm)

--- pomodoro.py
+++ pomodoro.py
@@ -4,12 +4,30 @@ from tqdm import tqdm
 def send_notification(text):
     subprocess.run(['notify-send', text])
 
+
+def set_terminal_title(title):
+    print(f'\033]2;{title}\a')
+
+def format_remaining_str(remaining, prefix="Pomodoro timer. Time remaining: "):
+    seconds = remaining % 60
+    minutes = remaining // 60
+    hours = remaining // 360
+
+    l = [minutes, seconds]
+    if hours > 0:
+        l.insert(0, hours)
+
+    return prefix + ':'.join(map(lambda x: str(x).zfill(2), l))
+
+
 def timer(minutes=0, seconds=0):
     seconds += 60 * minutes
-    for _ in tqdm(range(seconds),
+    set_terminal_title(format_remaining_str(seconds))
+    for r in tqdm(range(seconds-1, -1, -1),
             desc="Progress: ",
             bar_format='{desc} |{bar}| ({remaining} remaining)'):
         time.sleep(1.0)
+        set_terminal_title(format_remaining_str(r))
     send_notification("Timer has run out")
 
 if __name__ == '__main__':
